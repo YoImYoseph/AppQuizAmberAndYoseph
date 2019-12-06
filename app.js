3bb8dba01735b3c3805898d085b27267e8d1815e
Yoseph Student <blsck1024@gmail.com>
2019-12-06 11:46:41 -0600

    added app.js

--- app.js
+++ app.js
@@ -1,33 +1,90 @@
+document.documentElement.className = 'no-fouc';
 /**
  * Example store structure
  */
 const store = {
   // 5 or more questions are required
+  beginingP: 0,
+  currentA: null,
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'Billie Eilish been writing music since she was ___.',
+      correctAnswer: 2,
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
+        5,
+        9,
+        11,
+        16
+      ]
     },
+
     {
-      question: 'What is the current year?',
+      question: '___ started rapping under the name “JayOhVee”, but eventually changed it to ___.',
+      correctAnswer: 3,
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
+        'Lil Uzi',
+        '21 Savage',
+        '6IX9INE',
+        'Joey Bada$$'
+      ]
+    },
+
+    {
+      question: 'Lil Yachty used to work at ___.',
+      correctAnswer: 3,
+      answers: [
+        'Bdubs',
+        'Red Lobster',
+        'Taco Bell',
+        'McDonald’s'
+      ]
+    },
+    
   ],
   questionNumber: 0,
   score: 0
 };
+
+function renderQA(){
+	var beginingP = STORE.beginingP;
+	$('#count').text('Question '+beginingP+' of '+(STORE.questions.length-2));
+	$('#question').text(STORE.questions[beginingP].q);
+	$('#result').hide();
+  initSelection();
+  
+if(beginingP == 0){
+	$('#count').css('visibility', 'hidden');
+		STORE.questions[beginingP].a.forEach(function(answer, index){
+			var introHTML = `<p>${answer}</p>`;
+	$('#intro').append(introHTML);
+		});
+	$('.radio-item').hide();
+	$('#button-submit').show().text('Begin');
+	$('#button-continue').hide();
+	}
+
+else if(currentQ == 11){ // END
+	$('#count').css('visibility', 'hidden');
+	getResults();
+	for (i = 1; i < STORE.questions.length-1; i++){ // Render answers to radio labels
+		var results = {
+			n: i,
+			q: STORE.questions[i].q,
+			c: STORE.questions[i].a[STORE.questions[i].c],
+			u: STORE.questions[i].a[STORE.questions[i].u],
+			r: STORE.questions[i].r
+    }
+			var resultsHTML = `<p class="results-p"><span class="results-number">${results.n}.</span> <span class="results-question">${results.q}</span><br>Correct Answer: <span class="results-correct">${results.c}</span><br>Your Answer: <span class="results-user">${results.u}</span><br>Result: <span class="results-result">${results.r}</span></p>`;
+			$('.radio-item').hide();
+			$('#end-results').hide();
+			$('#end-results').append(resultsHTML);
+			$('#end-results').fadeIn();
+			$('#button-submit').show().text('Restart');
+			$('#button-continue').hide();
+		};
+	}
+
+
 
 /**
  *
